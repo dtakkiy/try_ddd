fe0451ab67edf813f9621a8d44b3f36af63c2c5f
dtakkiy <dtakkiy00@gmail.com>
2022-04-08 04:26:54 +0900

    feat: 存在するペア名を取得するメソッドを追加

--- src/domain/team/pair-same-name-exists.ts
+++ src/domain/team/pair-same-name-exists.ts
@@ -1,27 +1,33 @@
 import { ITeamRepository } from './team-repository-interface';
 
 interface Props {
-  pairName: string;
   repository: ITeamRepository;
 }
 
 export class PairSameNameExist {
   private readonly repository: ITeamRepository;
-  private readonly pairName: string;
 
   constructor(props: Props) {
     this.repository = props.repository;
-    this.pairName = props.pairName;
   }
 
-  public async execute(teamId: string): Promise<boolean> {
+  public async isPairName(pairName: string, teamId: string): Promise<boolean> {
     const team = await this.repository.getById(teamId);
     if (team === null) {
       return false;
     }
 
     return team.getPairList().some((pair) => {
-      pair.name.getValue() === this.pairName;
+      pair.name.getValue() === pairName;
     });
+  }
+
+  public async getPairNameListByTeamId(teamId: string): Promise<string[]> {
+    const team = await this.repository.getById(teamId);
+    if (team === null) {
+      return [];
+    }
+
+    return team.getPairList().map((pair) => pair.name.getValue());
   }
 }
