279b3ee6271d127a564ec290ac90fd4072d7c687
dtakkiy <dtakkiy00@gmail.com>
2022-04-14 09:09:39 +0900

    feat: コントローラにページング情報を返す処理を追加

--- src/controller/response/get-search-response.ts
+++ src/controller/response/get-search-response.ts
@@ -1,12 +1,21 @@
 import { ApiProperty } from '@nestjs/swagger';
 import { SearchDTO } from 'src/app/query-service-interface/search-task-query-service';
+
+import { Page, Paging } from 'src/domain/__shared__/page';
+
 export class GetSearchResponse {
   @ApiProperty({ type: () => [SearchData] })
   searchData: SearchData[];
+  pagingData: Paging = {
+    totalCount: 0,
+    pageNumber: 0,
+    pageSize: 0,
+  };
 
-  public constructor(params: { searchDatas: SearchDTO[] }) {
+  public constructor(params: { searchDatas: Page<SearchDTO> }) {
     const { searchDatas } = params;
-    this.searchData = searchDatas.map(({ id, name, email }) => {
+    this.pagingData = searchDatas.paging;
+    this.searchData = searchDatas.items.map(({ id, name, email }) => {
       return new SearchData({
         id,
         name,
