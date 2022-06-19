3a5953547c236efdb9f586dad105ddce45dec2e5
dtakkiy <dtakkiy00@gmail.com>
2022-06-19 09:57:34 +0900

    feat: 異常系のテストケースを追加

--- src/app/__tests__/get-member-list-usecase.integrate.spec.ts
+++ src/app/__tests__/get-member-list-usecase.integrate.spec.ts
@@ -2,7 +2,7 @@ import { PrismaClient } from '@prisma/client';
 import { MemberQueryService } from 'src/infra/db/query-service/member-query-service';
 import { GetMemberListUseCase } from '../get-member-list-usecase';
 
-describe('【ユースケース】結合テスト 参加者一覧を取得する', () => {
+describe('【ユースケース 結合テスト】 参加者一覧を取得する', () => {
   let prisma: PrismaClient;
 
   beforeEach(() => {
@@ -13,16 +13,17 @@ describe('【ユースケース】結合テスト 参加者一覧を取得する
     await prisma.$disconnect();
   });
 
-  it('[正常系] 結合テスト 参加者の一覧を取得する', async () => {
-    const memberQueryService = new MemberQueryService(prisma);
-    const usecase = new GetMemberListUseCase(memberQueryService);
-    return await expect(usecase.execute()).resolves.toBe(undefined);
-  });
-
-  it('結合テスト 参加者一覧の件数を検証', async () => {
+  it('【正常系】参加者一覧の件数を検証', async () => {
     const memberQueryService = new MemberQueryService(prisma);
     const usecase = new GetMemberListUseCase(memberQueryService);
     const memberDTO = await usecase.execute();
     expect(memberDTO.length).toBe(4);
+  });
+
+  it('【異常系】参加者一覧の件数を検証', async () => {
+    const memberQueryService = new MemberQueryService(prisma);
+    const usecase = new GetMemberListUseCase(memberQueryService);
+    const memberDTO = await usecase.execute();
+    expect(memberDTO.length).not.toBe(40);
   });
 });
